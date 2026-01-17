c45231a4e798dd408cf778e71038e24bfdb4cd7d
Yao Yao <yao.yao@phyworks.cn>
2026-01-17 15:51:51 +0800

    add ga

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -5,11 +5,16 @@ import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
 // @ts-ignore
 import zhCn from 'element-plus/dist/locale/zh-cn.mjs'
+import VueGtag from 'vue-gtag-next'
 
 const app = createApp(App)
 
 app.use(ElementPlus, {
   locale: zhCn,
+})
+
+app.use(VueGtag, {
+  property: { id: "G-Q8NQSMH4S8" } // 请替换为您真实的 GA4 衡量 ID
 })
 
 app.mount('#app')
